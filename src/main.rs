8251a22069bf4013191d4e5164a0a03f1f96473b
Tom <tjpalmer@tjpalmer.com>
2020-06-21 14:55:19 -0700

    Go back to wasmparser

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,57 @@
-mod load;
 mod util;
 use std::{fs::File, io::prelude::*};
 use util::*;
-// use wasmparser::{Parser, ParserState, WasmDecoder};
+use wasmparser::{Parser, ParserState, WasmDecoder};
 
 fn main() -> Try<()> {
     let buf = read_bytes()?;
-    load::run(&buf)?;
-    // if false {
-    //     let mut parser = Parser::new(&buf);
-    //     loop {
-    //         let state = parser.read();
-    //         if !handle_state(state) {
-    //             break;
-    //         }
-    //     }
-    // }
+    if true {
+        let mut parser = Parser::new(&buf);
+        loop {
+            let state = parser.read();
+            if !handle_state(state) {
+                break;
+            }
+        }
+    }
     Ok(())
 }
 
-// fn handle_state(state: &ParserState) -> bool {
-//     match *state {
-//         ParserState::BeginFunctionBody { .. } => {
-//             println!("Begin function body");
-//         }
-//         ParserState::BeginWasm { .. } => {
-//             println!("====== Module");
-//         }
-//         ParserState::EndFunctionBody => {
-//             println!("End function body");
-//         }
-//         ParserState::EndWasm => {
-//             return false;
-//         }
-//         ParserState::ExportSectionEntry {
-//             field,
-//             ref kind,
-//             index,
-//         } => {
-//             println!("Export {} {:?} at {}", field, kind, index);
-//         }
-//         ParserState::ImportSectionEntry { module, field, ty } => {
-//             // wasmparser::ImportSectionEntryType
-//             println!("Import {}::{} of {:?}", module, field, ty);
-//         }
-//         ParserState::TypeSectionEntry(ref func_type) => {
-//             // wasmparser::FuncType
-//             println!("Type section entry: {:?}", func_type);
-//         }
-//         _ => {
-//             println!("Other {:?}", state);
-//         }
-//     }
-//     true
-// }
+fn handle_state(state: &ParserState) -> bool {
+    match state {
+        ParserState::BeginFunctionBody { .. } => {
+            println!("Begin function body");
+        }
+        ParserState::BeginWasm { .. } => {
+            println!("====== Module");
+        }
+        ParserState::EndFunctionBody => {
+            println!("End function body");
+        }
+        ParserState::EndWasm => {
+            return false;
+        }
+        ParserState::ExportSectionEntry {
+            field,
+            ref kind,
+            index,
+        } => {
+            println!("Export {} {:?} at {}", field, kind, index);
+        }
+        ParserState::ImportSectionEntry { module, field, ty } => {
+            // wasmparser::ImportSectionEntryType
+            println!("Import {}::{} of {:?}", module, field, ty);
+        }
+        ParserState::TypeSectionEntry(ref func_type) => {
+            // wasmparser::FuncType
+            println!("Type section entry: {:?}", func_type);
+        }
+        _ => {
+            println!("Other {:?}", state);
+        }
+    }
+    true
+}
 
 fn read_bytes() -> Try<Vec<u8>> {
     let args: Vec<String> = std::env::args().collect();
